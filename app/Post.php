9438d23dae1a39fba703fb6c8be0f193ce1544f1
matsumi-soccer <my18847matsu@softbank.ne.jp>
2022-08-15 22:33:03 +0900

    08-2

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,10 @@ class Post extends Model
         //updateed_atで降順に並べたあと、limitで制限をかける
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        //updated_atで降順に並べた後、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
