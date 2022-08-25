1318465016bd621b92f5add2c88f9ce56c387486
matsumi-soccer <my18847matsu@softbank.ne.jp>
2022-08-25 17:50:51 +0900

    09-2

--- app/Http/Controllers/PostContoller.php
+++ app/Http/Controllers/PostContoller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use App\Http\Requests\PostRequest;
+use App\Category;
 
 class PostContoller extends Controller
 {
@@ -17,9 +18,9 @@ class PostContoller extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts/create');
+        return view('posts/create')->with(['categories'=>$category->get()]);
     }
     
     public function store(Post $post, PostRequest $request)
@@ -39,6 +40,12 @@ class PostContoller extends Controller
         $input_post = $request['post'];
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
 ?>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,12 +10,14 @@
     </head>
     <body>
         <p class="edit">[<a href="/posts/{{$post->id}}/edit">edit</a>]</p>
+        <a href ="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
         <h1 class="title">
             {{$post->title}}
         </h1>
         <div class="content">
             <div class="content_post">
                 <h3>本文</h3>
+                <a href = "">{{$post->category->name}}</a>
                 <p>{{$post->body}}</p>
             </div>
         </div>
